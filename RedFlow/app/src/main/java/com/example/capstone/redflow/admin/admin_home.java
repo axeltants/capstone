42a6abc87fe9456974fd3dc6dbaacb03c658b750
Axel <axeltantay@gmail.com>
2016-12-13 00:14:15 +0800

    search result list will now work

--- RedFlow/app/src/main/java/com/example/capstone/redflow/admin/admin_home.java
+++ RedFlow/app/src/main/java/com/example/capstone/redflow/admin/admin_home.java
@@ -20,21 +20,22 @@ public class admin_home extends AppCompatActivity {
 
     public void statistics(View view) {
         Toast.makeText(getApplicationContext(), "Statistics", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, history.class);
+        Intent intent = new Intent(this,  admin_home.class);
     }
 
     public void donors(View view) {
         Toast.makeText(getApplicationContext(), "Donors", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, history.class);
+        Intent intent = new Intent(this, admin_home.class);
     }
 
     public void records(View view) {
         Toast.makeText(getApplicationContext(), "records", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, history.class);
+        Intent intent = new Intent(this,  admin_home.class);
     }
 
     public void searchuser(View view) {
         Toast.makeText(getApplicationContext(), "search user", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, history.class);
+        Intent intent = new Intent(this, search_user.class);
+        startActivity(intent);
     }
 }
--- RedFlow/app/src/main/java/com/example/capstone/redflow/admin/search_user.java
+++ RedFlow/app/src/main/java/com/example/capstone/redflow/admin/search_user.java
@@ -1,9 +1,12 @@
 package com.example.capstone.redflow.admin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.capstone.redflow.R;
+import com.example.capstone.redflow.request;
 
 public class search_user extends AppCompatActivity {
 
@@ -11,5 +14,10 @@ public class search_user extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_user);
+    }
+
+    public void search(View view) {
+        Intent intent = new Intent(this, search_result.class);
+        startActivity(intent);
     }
 }
