64a4961d434c551bd9b5d168a781b2ef2f1c2b40
Axel <axeltantay@gmail.com>
2016-12-18 16:32:11 +0800

    history home activity & UI fix

--- RedFlow/app/src/main/java/com/example/capstone/redflow/history.java
+++ RedFlow/app/src/main/java/com/example/capstone/redflow/history.java
@@ -3,6 +3,7 @@ package com.example.capstone.redflow;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class history extends AppCompatActivity {
 
@@ -15,5 +16,11 @@ public class history extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(com.example.capstone.redflow.R.menu.actionbar, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void blooddonation_list(View view) {
+    }
+
+    public void bloodclaim_list(View view) {
     }
 }
