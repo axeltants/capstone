4758fe4577d59b8c880b7f68b8653103191acc0f
Axel <axeltantay@gmail.com>
2017-01-06 21:24:41 +0800

    remove multidex to speedup the app

--- RedFlow/app/src/main/java/com/example/capstone/redflow/register.java
+++ RedFlow/app/src/main/java/com/example/capstone/redflow/register.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.multidex.MultiDex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
