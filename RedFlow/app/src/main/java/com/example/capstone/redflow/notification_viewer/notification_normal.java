c368f539dc2a237a7a2f06b8daf88f248d34dbbe
Axel <axeltantay@gmail.com>
2017-02-09 20:53:02 +0800

    pass user id to notification viewers

--- RedFlow/app/src/main/java/com/example/capstone/redflow/notification_viewer/notification_normal.java
+++ RedFlow/app/src/main/java/com/example/capstone/redflow/notification_viewer/notification_normal.java
@@ -1,6 +1,8 @@
 package com.example.capstone.redflow.notification_viewer;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +14,8 @@ import com.example.capstone.redflow.preliminary_bloodtest.TestActivity;
 
 import java.util.Calendar;
 
+import static com.example.capstone.redflow.common_activities.LoginActivity.Uid;
+
 public class notification_normal extends AppCompatActivity {
 
     TextView title;
@@ -21,11 +25,19 @@ public class notification_normal extends AppCompatActivity {
     private String notiftitle;
     private String notifcontent;
     private String notifdate;
+    private String uid;
+
+    SharedPreferences sharedpreferences;
+    public static final String MyPREFERENCES = "MyPrefs" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification_normal);
+
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        uid = sharedpreferences.getString(Uid, ""); // MAU NI NGA VARIABLE GAMITA TER.
+
 
         title = (TextView) findViewById(R.id.notifTitle);
         content = (TextView) findViewById(R.id.notifContent);
--- RedFlow/app/src/main/java/com/example/capstone/redflow/notification_viewer/notification_with_prelim.java
+++ RedFlow/app/src/main/java/com/example/capstone/redflow/notification_viewer/notification_with_prelim.java
@@ -1,6 +1,8 @@
 package com.example.capstone.redflow.notification_viewer;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,8 @@ import com.example.capstone.redflow.preliminary_bloodtest.TestActivity;
 
 import java.util.Calendar;
 
+import static com.example.capstone.redflow.common_activities.LoginActivity.Uid;
+
 public class notification_with_prelim extends AppCompatActivity {
 
 
@@ -22,11 +26,18 @@ public class notification_with_prelim extends AppCompatActivity {
     private String notiftitle;
     private String notifcontent;
     private String notifdate;
+    private String uid;
+
+    SharedPreferences sharedpreferences;
+    public static final String MyPREFERENCES = "MyPrefs" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification_with_prelim);
+
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        uid = sharedpreferences.getString(Uid, ""); // MAU NI NGA VARIABLE GAMITA TER
 
         title = (TextView) findViewById(R.id.notifTitle);
         content = (TextView) findViewById(R.id.notifContent);
